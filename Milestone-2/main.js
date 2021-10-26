bfcccf97339d7de40bb67ffb3c05e3ccd7d816df
Andretreccia <andreabtreccia@gmail.com>
2021-10-26 16:49:11 +0200

    Problema nell aggiunta colore per chiave oggetto

---
+++ Milestone-2/main.js
@@ -0,0 +1,128 @@
+const allIcons = [
+    {
+        name: 'cat',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'crow',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'dog',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'dove',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'dragon',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'horse',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'hippo',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'fish',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'carrot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'apple-alt',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'lemon',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'pepper-hot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'user-astronaut',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    },
+    {
+        name: 'user-graduate',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    },
+    {
+        name: 'user-ninja',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    },
+    {
+        name: 'user-secret',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    }
+];
+
+//con un cilo aggiungere le card a schermo
+allIcons.forEach((element, index, array) => {
+    let cardIcon = `<div class="card">
+                        <i class="${element.family} ${element.prefix}${element.name} color"></i>
+                        <h6>${element.name}</h6>
+                    </div>`
+    //console.log(cardIcon)
+    let rowCards = document.querySelector(".row");
+    rowCards.innerHTML += cardIcon
+    if (element.type == 'user') {
+        document.querySelectorAll(".color").className += "purple"
+    }
+})
+
+//funzione per colorare
+//selezionare tutti gli oggetti con chiave type
+/* allIcons.forEach((element) => {
+    if (element.type == 'user') {
+        let cardIconDOM = document.querySelectorAll(".fa-");
+        cardIconDOM.classList.add("purple");
+    }
+})
+console.log(cardIconDOM) */
+    //se la chiave ha valore user
+        //aggiungi classe colore
+    //se la cchiave ha valore vegetable
+        //aggiungi classe colore
+    //se la chiave ha valore animal
+        //aggiungi classe colore
